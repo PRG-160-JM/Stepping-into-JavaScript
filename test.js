531450faa7b2e762c84770a57c051abf4b96bee1
jonathanmiller <penguinsj71@comcast.net>
2018-02-10 19:45:37 -0500

    Deleted previous content and add content for assignment011.

--- test.js
+++ test.js
@@ -1,30 +1,20 @@
-function getAverage (a,b,c,d,e,f){
+var average = 0;
+
+function getAverage (a,b){
 	
-	var average = (a + b + c + d + e + f) / 6;
+	var average = (a + b) / 2; //local variable
 	console.log(average);
 	return average;
 	
 }
 
-var myResult = getAverage(7,8,9,10,11,12);
+var myResult = getAverage(7,11); //global variable
 console.log("the average is " + myResult);
 
-function getDifference (a,b){
+function logResult(){
 	
-	var difference = a / b;
-	console.log(difference);
-	return difference;
+	console.log("the average is " + myResult + " inside the function")
+	
 }
 
-var myResult = getDifference(6,2);
-console.log("the difference is " + myResult);
-
-function getStr(str1,str2){
-	
-	var res = str1.concat(str2);
-	console.log(res);
-	return res;
-}
-
-var myResult = getStr(a + b);
-console.log("the full string is " +myResult); 
+logResult();
