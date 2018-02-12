a95eaf53d0d97accc63d499dcf95915447e7ac87
jonathanmiller <penguinsj71@comcast.net>
2018-02-11 19:58:53 -0500

    Added contents for checkpoint 2.

--- JSChallenge001.js
+++ JSChallenge001.js
@@ -8,11 +8,32 @@ function getPrimeNum(value)
     for(var num = 2; num < value; num++) 
 	{
         if(value % num === 0) 
-		{
-            return false;
-        }
+			{
+				return false;
+			}
     }
     return value > 1;
 }
 
 console.log(getPrimeNum(56));
+
+/*Checkpoint 2 */
+
+function getYear(value)
+{
+	var year = 1996; year++
+	{
+		if(value >= year)
+			{
+				document.write("You are too young!")
+				
+			}
+		else if(value <= year)
+			{		
+				document.write("You are old enough, turn up!")
+				
+			}
+	}
+	
+}
+console.log(getYear(1995));
